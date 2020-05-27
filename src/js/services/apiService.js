0dd1d4ff99e3b1648b2046eed5b03cfed5252ebf
iv <consmebius@gmail.com>
2020-05-27 23:19:39 +0300

    add store..

---
+++ src/js/services/apiService.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import config from '../config/apiConfig';
+
+/**
+ * /countries - вернет массив стран
+ * /cities - вернет массив городов
+ * /prices/cheap - вернет массив с рейсами
+ */
+
+class Api {
+    constructor(config) {
+        this.url = config.url;
+    }
+    async countries() {
+        try {
+            const response = await axios.get(`${this.url}/countries`);
+            // console.log(response);
+            return response.data
+        } catch (error) {
+            console.log(error);
+            return Promise.reject(error);
+        }
+    }
+    async cities() {
+        try {
+            const response = await axios.get(`${this.url}/cities`);
+            // console.log(response);
+            return response.data
+        } catch (error) {
+            console.log(error);
+            return Promise.reject(error);
+        }
+    }
+    async prices(parm) {}
+
+}
+
+const api = new Api(config);
+export default api;
